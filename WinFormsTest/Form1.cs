f410fcb5418467e48e3591f90259c39a73feba35
Bob Voigt <bobv@restechimage.com>
2019-02-19 14:56:02 -0500

    WinForms. Halcon 12.

---
+++ WinFormsTest/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace WinFormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            StartupLicenseTest();
+        }
+        private bool StartupLicenseTest()
+        {
+            bool status = false;
+
+            HImage testImage = new HImage();
+            HRegion testRegion = new HRegion();
+
+            try
+            {
+                // This will fail if we don't have a proper license
+                testImage = new HImage("byte", 16, 16);
+                testRegion = testImage.Threshold(0.0, 255.0);
+
+                HSystem.SetSystem("temporary_mem_cache", "false");  // This saves RAM significantly!
+                HSystem.SetSystem("global_mem_cache", "idle");      // Likewise
+
+                status = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " " + DateTime.Now);    // Display errors if any in a listbox
+            }
+            finally
+            {
+                testImage.Dispose();
+                testRegion.Dispose();
+            }
+
+            return status;
+        }
+
+    }
+}
---
+++ WinFormsTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace WinFormsTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                HSystem.GetSystem("version");
+            }
+            catch (HOperatorException hEx)
+            {
+                var messageBoxResult = MessageBox.Show($"HALCON unable to initialize. Please check if license and dongle are installed correctly.\r\n{hEx.Message}", "HALCON Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(hEx.GetErrorCode());
+            }
+            Application.Run(new Form1());
+        }
+    }
+}
